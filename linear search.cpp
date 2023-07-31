15a746c1cd72a5ae55ce51163440e1207b3b0fb2
Pratikbarai <103105372+Pratikbarai@users.noreply.github.com>
2023-07-31 09:40:58 +0530

    Update linear search.cpp

--- linear search.cpp
+++ linear search.cpp
@@ -12,21 +12,21 @@ int linear_search(int a[],int size,int value)
                
         }
         if(j)    
-            cout<<"Element found";
+            cout<<"Element found"<<endl;
         else
-                cout<<"Element not found"; 
+                cout<<"Element not found"<<endl; 
         return 0;
 }
 int main()
 {
     int size,i,value;
-    cout<<"enter the size of array: ";
+    cout<<"enter the size of array: "<<endl;
     cin>>size;
-    cout<<"enter the elements of array: ";
+    cout<<"enter the elements of array: "<<endl;
     int array[size];
     for(i=0;i<size;i++)
         cin>>array[i];
-    cout<<"Enter the search element: ";
+    cout<<"Enter the search element: "<<endl;
     cin>>value;
     linear_search(array,size,value);
     return 0;
